79d07c9fd09f76406332872f68d7f25335fca490
qingwuz <37654692+qingwuz@users.noreply.github.com>
2019-04-23 11:17:31 +0800

    Update attack.py

--- attack.py
+++ attack.py
@@ -1,9 +1,11 @@
 #NonTargetedAttack.py
 import numpy
 import sys
-import PIL.Image
 import PIL.ImageFile
 import PIL.ImageFilter
+from PIL import Image
+from numpy import *
+from scipy.ndimage import filters
 
 (PIL.ImageFile).LOAD_TRUNCATED_IMAGES = True
 NUMBER_OF_CLASSES = 110
@@ -16,32 +18,21 @@ File = open(InputDirectory + "/dev.csv")
 File.readline()
 
 for line in File:
-	split_line = line.strip().split(",")
+    split_line = line.strip().split(",")
 
-	filename = split_line[0]
-	true_label = int(split_line[1])
-	targeted_label = int(split_line[2])
+    filename = split_line[0]
+    true_label = int(split_line[1])
+    targeted_label = int(split_line[2])
 
-	image_pil = PIL.Image.open(InputDirectory + "/" + filename)
-	image = numpy.asarray(image_pil.resize([SIZE, SIZE], PIL.Image.BILINEAR).convert("RGB")).astype(numpy.float32)
+    image_pil = array(PIL.Image.open(InputDirectory + "/" + filename))
 
-	left = 45
-	right = 260
-	top = 45
-	bottom = 260
+    image2 = zeros(image_pil.shape)
+    for i in range(3):
+        image2[:,:,i] = filters.gaussian_filter(image_pil[:,:,i],4)
+    image = uint8(image2)
+    new_image = Image.fromarray(image)
+    #new_image = new_image.astype(numpy.uint8)
 
-	new_image = image.copy()
-	n = 1
-	for i in range(36):
-		new_image[left:right, top:bottom] += image[(left - i):(right - i), top:bottom]
-		new_image[left:right, top:bottom] += image[(left + i):(right + i), top:bottom]
-		new_image[left:right, top:bottom] += image[left:right, (top - i):(bottom - i)]
-		new_image[left:right, top:bottom] += image[left:right, (top + i):(bottom + i)]
-		n += 4
-
-	new_image[left:right, top:bottom] /= n
-	new_image = new_image.astype(numpy.uint8)
-
-	PIL.Image.fromarray(numpy.asarray(new_image, numpy.int8), "RGB").save(OutputDirectory + "/" + filename)
+    PIL.Image.fromarray(numpy.asarray(new_image, numpy.int8), "RGB").save(OutputDirectory + "/" + filename)
 
 File.close()
