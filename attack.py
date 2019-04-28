13fced629de7a120442d1c2518b89f61acd623ff
qingwuz <37654692+qingwuz@users.noreply.github.com>
2019-04-28 08:16:26 +0800

    Update attack.py

--- attack.py
+++ attack.py
@@ -27,9 +27,9 @@ for line in File:
     image_pil = array(PIL.Image.open(InputDirectory + "/" + filename))
     
     left = 20
-    right = 269
+    right = 299
     top = 20
-    bottom = 269
+    bottom = 299
     
     image2 = image_pil.copy()#zeros(image_pil.shape)
     for i in range(3):
